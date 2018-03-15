386c5a8709215f1745586ea073a037532e78bf2c
jamclaub <33885324+jamclaub@users.noreply.github.com>
2018-03-14 21:15:19 -0500

    New Classes added for structure

---
+++ 2018 Group Project/ConcreteUnitcreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_Group_Project
+{
+    class ConcreteUnitcreator: Unitcreator
+    {
+        public override void CreateInfantry()
+        {    
+        }
+        public override void CreateVehicle()
+        {   
+        }
+        public override void CreateWalker()
+        {
+        }
+    }
+}
---
+++ 2018 Group Project/Unitcreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_Group_Project
+{
+    abstract class Unitcreator
+    {
+        abstract public void CreateVehicle();
+        abstract public void CreateInfantry();
+        abstract public void CreateWalker();
+    }
+}
